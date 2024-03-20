c5e97b6651da88f09bc7fcb5867922251f2134a9
Trần Minh Hiểu <131644516+Tmh3101@users.noreply.github.com>
2024-03-20 16:03:16 +0700

    Update 02_InsertionSort.c

--- SortAlgorithm/02_InsertionSort.c
+++ SortAlgorithm/02_InsertionSort.c
@@ -17,10 +17,10 @@ void swap(recordtype *a, recordtype *b){
 	*b = tmp;
 }
 
-void insertionSort(recordtype a[], int n){
-	int i, j;
+void insertion_sort(recordtype *a, int n){
+	int i;
 	for(i = 1; i < n; i++){
-		j = i;
+		int j = i;
 		while(j > 0 && a[j].key < a[j - 1].key){
 			swap(&a[j], &a[j - 1]);
 			j--;
@@ -28,31 +28,42 @@ void insertionSort(recordtype a[], int n){
 	}
 }
 
+void read_file(recordtype *a, int *n){
+	FILE *f = fopen("data.txt", "r");
+	if(f == NULL){
+		printf("Error\n");
+		return;
+	}
+	
+	int i = 0;
+	while(fscanf(f, "%d %f", &a[i].key, &a[i].otherfields) > 0){
+		i++;
+	}
+	fclose(f);
+	*n = i;
+}
+
+void print_data(recordtype *a, int n){
+	int i;
+	for(i = 0; i < n; i++){
+		printf("%3d %5d  %8.2f\n", i, a[i].key, a[i].otherfields);
+	}
+}
 
 int main(){
 	recordtype a[100];
-	int n = 0;
-	int key;
-	float otherfields;
-
-	freopen("data.txt", "r", stdin);
-	do {
-		key = INT_MIN;
-		otherfields = FLT_MIN;
-		scanf("%d%f", &key, &otherfields);
-		if (key != INT_MIN && otherfields != FLT_MIN){
-			a[n].key = key;
-			a[n].otherfields = otherfields;
-			n++;
-		}
-	} while (key != INT_MIN && otherfields != FLT_MIN);
-
-	insertionSort(a, n);
-
-	int i;
-	for(i = 0; i < n; i++){
-		printf("%d %.2f\n", a[i].key, a[i].otherfields);
-	}
+	int n ;
+	
+	printf("---Insertion Sort---\n");
+	read_file(a, &n);
+	
+	printf("* Truoc khi sap xep:\n");
+	print_data(a, n);
+	
+	insertion_sort(a, n);
+	
+	printf("* Sau khi sap xep:\n");
+	print_data(a, n);
 	
 	return 0;
 }
